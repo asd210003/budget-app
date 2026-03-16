4477c9505e6c1d4ef7249294349e099e0e8e0ab8
adenni62 <adenni62@wgu.edu>
2026-03-15 19:37:37 -0500

    Initialize backend spring to project

---
+++ budget-back-end/src/main/java/org/hello/budgetbackend/BudgetBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.hello.budgetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetBackEndApplication.class, args);
+    }
+
+}
---
+++ budget-back-end/src/test/java/org/hello/budgetbackend/BudgetBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hello.budgetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
