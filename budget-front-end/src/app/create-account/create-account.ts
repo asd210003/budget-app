4f6206fba12379869f213ee7fe7f17dc60fa5e95
adenni62 <adenni62@wgu.edu>
2026-03-15 19:35:04 -0500

    Initialize back-end spring into project

---
+++ budget-front-end/src/app/create-account/create-account.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-account',
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './create-account.html',
+  styleUrl: './create-account.css',
+})
+export class CreateAccount {
+
+  constructor(private router: Router) {}
+
+  submitted = false;
+
+  formValid = new FormGroup({
+      firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      cPassword: new FormControl('', [Validators.required])
+  });
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.formValid.invalid) {
+      return;
+    }
+
+    this.router.navigate(['/home']);
+  }
+
+  get firstName() {
+    return this.formValid.get('firstName');
+  }
+
+  get username() {
+    return this.formValid.get('username');
+  }
+
+  get email() {
+    return this.formValid.get('email');
+  }
+
+  get password() {
+    return this.formValid.get('password');
+  }
+
+  get cPassword() {
+    return this.formValid.get('cPassword');
+  }
+
+}
